871c346dd246cee179f0846d61e5dde7f66feca3
gatwi <faith.murithi@student.moringaschool.com>
2022-07-27 00:55:34 +0300

    send commit

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -3,14 +3,17 @@ import { Link } from "react-router-dom";
 
  function Navbar(){
     return(
-        <nav className="navbar bg-dark">
+        <nav className="navbar  bg-dark">
             <div className="container-fluid">
-               <ul className="navbar-nav">
+               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                    <li className="nav-item">
                      <Link className="nav-link" to="/">HOME</Link>
                     </li>
                     <li className="nav-item">
                      <Link className="nav-link" to="/search">SEARCH</Link>
+                    </li>
+                    <li className="nav-item">
+                     <Link className="nav-link" to="/">CATALOGUE</Link>
                     </li>
                 </ul>
             </div>
