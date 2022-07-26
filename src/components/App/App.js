f86495ebf43beff9a52e1810d100506a683efce2
gatwi <faith.murithi@student.moringaschool.com>
2022-07-26 12:15:28 +0300

    trying to incorporate an external API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../NavBar/Navbar";
-import { BrowserRouter as Router } from "react-router-dom"
+import Home from "../Home/Home";
+import Search from "../Search/Search";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+
+const API_URL = "www.thecocktaildb.com/api/json/v1/1/search.php";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [cocktails, setCocktails] = useState([]);
+
+  useEffect(() => {
+    searchCocktails("");
+  }, []);
+  
+  const searchCocktails = async (name) => {
+    const response = await fetch(`${API_URL}&s={name}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+  useEffect(() => {
+    searchCocktails("");
+  }, []);
+
   return (
     <Router>
       <div>
        <Navbar/>
       </div>
+      <Routes>
+        <Route exact path="/" element={<Home/>}></Route>
+        <Route exact path="/search" element={<Search/>}></Route>
+      </Routes>
     </Router>
   );
 }
--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Navbar(){
+ function Navbar(){
     return(
         <nav className="navbar bg-light">
-            <ul className="navbar-nav">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/search">Search</Link>
-                </li>
-            </ul>
+            <div className="container-fluid">
+               <ul className="navbar-nav">
+                   <li className="nav-item">
+                     <Link className="nav-link" to="/">HOME</Link>
+                    </li>
+                    <li className="nav-item">
+                     <Link className="nav-link" to="/search">SEARCH</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
 
+export default Navbar;
