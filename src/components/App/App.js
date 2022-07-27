5b3bdf3f66d6e817c84602359d264213ad331ee1
gatwi <faith.murithi@student.moringaschool.com>
2022-07-27 09:04:38 +0300

    enter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,7 @@ import "./../../App.css";
 import SearchIcon from "./../../search.svg";
 import BooksCard from "./../Items/BooksCard";
 
-const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail";
+// const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail";
 
 
 
@@ -16,36 +16,42 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   
 
-
-
- 
   useEffect (() => {
-    fetch(API_URL)
+    fetch("http://localhost:3000/books")
     .then(response => response.json())
     .then(response => console.log(response))
   }, []);
 
 
   return (
-    <div className="app">
-      <h1>The Winebrary</h1>
-      
+      <Router>
+        <div>
+          <Navbar/>
+        </div>
+        <div className="app">
+         <h1>The Winebrary</h1>
+         
+          <div className="search">
+            <input
+             value={searchTerm}
+             onChange={(e) => setSearchTerm(e.target.value)}
+             placeholder="Keep searching..."
+            />
+            <img
+             src={SearchIcon}
+             alt="search"
+             onClick={() => {}}
+            />
+          </div>
 
-      <div className="search">
-        <input
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Keep searching..."
-        />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={() => {}}
-        />
-      </div>
+          <Routes>
+            <Route exact path="/" element={<Home/>}></Route>
+            <Route exact path="/" element={<Home/>}></Route>
+          </Routes>
 
-    </div>
-  );
+        </div>
+      </Router>
+   );
 };
   
 
--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
  function Navbar(){
     return(
-        <nav className="navbar  bg-dark">
+        <nav className="navbar  bg-light">
             <div className="container-fluid">
                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                    <li className="nav-item">
