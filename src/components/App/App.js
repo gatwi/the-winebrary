7690f20f94c122b0a35de04c81ee5b1a80c0d741
gatwi <faith.murithi@student.moringaschool.com>
2022-07-28 13:14:07 +0300

    little css update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,13 @@ import CocktailsCard from "../Catalogue/CocktailsCard";
 import Navbar from "../NavBar/Navbar";
 import Home from "../Home/Home";
 import Catalogue from "../Catalogue/Catalogue";
-import Search from "../Search/Search";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./../../App.css";
-import SearchIcon from "./../../search.svg";
 import BooksCard from "../Catalogue/BooksCard";
 
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
+  
   
 
   useEffect (() => {
@@ -35,27 +33,16 @@ function App() {
       <div className="app">
         <h1>The Winebrary</h1>
       </div>
-      <div className="container">
-        <div className="search">
-           <input 
-           value={searchTerm} 
-           onChange={(e) => setSearchTerm(e.target.value)} 
-           placeholder="Keep searching..."
-         />
-         <img
-           src={SearchIcon}
-           alt="search"
-           onClick={() => {}}
-         />
+      
+
+      <div className="card-grid">
+        <div className="container-fluid">
+         <BooksCard bookTitle="The Book Thief" bookId="1" bookAuthor="Markus Zukas" bookUrl="https://images-na.ssl-images-amazon.com/images/I/91pZEozf9qL.jpg" />
         </div>
-      </div>
 
-      <div className="container-fluid">
-       <BooksCard bookTitle="The Book Thief" bookId="1" bookAuthor="Markus Zukas" bookUrl="https://images-na.ssl-images-amazon.com/images/I/91pZEozf9qL.jpg" />
-      </div>
-
-      <div className="container-fluid">
-        <CocktailsCard idDrink="14272" strDrink="Addison Special" strDrinkThumb="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/4vo5651493068493.jpg" />
+        <div className="container-fluid">
+         <CocktailsCard idDrink="14272" strDrink="Addison Special" strDrinkThumb="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/4vo5651493068493.jpg" />
+        </div>
       </div>
     </Router>
   )
--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import SearchIcon from "./../../search.svg";
+import Search from "../Search/Search";
 
  function Navbar(){
+    const [searchTerm, setSearchTerm] = useState("");
     return(
-        <nav className="navbar  bg-light">
-            <div className="container-fluid">
+        <nav className="navbar bg-light">
+            <div className="container">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
                      <Link className="nav-link" to="/">HOME</Link>
                     </li>
                     <li className="nav-item dropdown">
                         <Link className="nav-link dropdown-toggle" to="/CATALOGUE">CATALOGUE</Link>
-                        {/* <ul className="dropdown-menu">
-                            <li><Link className="dropdown-item">Books</Link></li>
-                            <li><Link className="dropdown-item">Cocktails</Link></li>
-                        </ul> */}
+                        <div className="d-flex">
+                            <div className="search">
+                                <input
+                                 value={searchTerm} 
+                                 onChange={(e) => setSearchTerm(e.target.value)} 
+                                 placeholder=""
+                               />
+                                <img
+                                 src={SearchIcon}
+                                 alt={Search}
+                                 onClick={() => {}}
+                               />
+                            </div>
+                        </div>
                     </li>
                 </ul>
             </div>
