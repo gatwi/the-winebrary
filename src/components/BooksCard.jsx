26c2db3a4c18a0b463579364d369415c266d745a
gatwi <faith.murithi@student.moringaschool.com>
2022-07-31 12:41:20 +0300

    add

--- src/components/BooksCard.jsx
+++ src/components/BooksCard.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import StarRating from "./StarRating";
 
 
-function BooksCard(){
+function BooksCard({addToFavorites}){
     const [books, setBooks] = useState([]);
+    const [isRead, setIsRead] = useState(false)
 
     useEffect(() => {
         fetch("http://localhost:3000/books")
@@ -15,9 +16,15 @@ function BooksCard(){
         <div className="card-grid" style={{width: 40 + 'rem'}}>
             <img src={book.Url} className="card-img-top" alt={book.Title}/>
             <div className="card-body">
-             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-             <StarRating />
-             <button className="btn btn-success">Add to Cart</button>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <StarRating />
+                <button className="btn btn-success" onClick={() => setIsRead(!isRead)} >
+                    Mark as {isRead ? "unread" : "read" }
+                </button>
+                <button className="btn btn-dark" onClick={() => addToFavorites(book)}>
+                    {book.isFavorite ? "Unfavorite" : "♥ Favorite"}
+                </button>
+                <button className="btn btn-primary btn-block">Add to Cart</button>
             </div>
         </div>
     ))
@@ -34,3 +41,5 @@ function BooksCard(){
 };
 
 export default BooksCard;
+
+
--- src/components/CocktailsCard.jsx
+++ src/components/CocktailsCard.jsx
@@ -5,8 +5,9 @@ import StarRating from "./StarRating";
 
 const cocktails_url = "http://localhost:3000/drinks"
 
-function CocktailsCard() {
+function CocktailsCard({addToFavorites}) {
     const [drinks, setDrinks] = useState([]);
+    
 
     useEffect(() => {
         fetch(cocktails_url)
@@ -18,9 +19,12 @@ function CocktailsCard() {
         <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4" style={{width: 18 + 'rem'}}>
             <img src={drink.strDrinkThumb} className="card-img-top" alt={drink.strDrink}/>
             <div className="card-body">
-             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-             <StarRating />
-             <button className="btn btn-success">Add to Cart</button>
+                <p className="card-text">{drink.strDrink}</p>
+                <StarRating />
+                <button className="btn btn-dark" onClick={() => addToFavorites(drink)}>
+                    {drink.isFavorite ? "Unfavorite" : "♥ Favorite"}
+                </button>
+                <button className="btn btn-success">Add to Cart</button>
             </div>
         </div>
     ))
